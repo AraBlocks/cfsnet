f71504f3286daa8ee83066fc2f993d01b40ecd73
Joseph Werle <joseph.werle@gmail.com>
2017-10-30 17:45:25 -0400

    cleanup and move cli stuff out of module

--- create-key-path.js
+++ create-key-path.js
@@ -2,7 +2,7 @@
 
 const { CFS_ROOT_DIR } = require('./env')
 const { createSHA256 } = require('./sha256')
-const { resolve } = require('path')
+const { resolve, join } = require('path')
 
 /**
  * This function creates a CFS key path from a given identifier
@@ -10,7 +10,7 @@ const { resolve } = require('path')
  * against the `CFS_ROOT_DIR` environment variable.
  */
 
-function createCFSKeyPath({id, key} = {}) {
+function createCFSKeyPath({id} = {}) {
   const hash = createSHA256(String(id))
   const path = resolve(CFS_ROOT_DIR, hash)
   return path
