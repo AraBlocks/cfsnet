49437ec73ffaaae3ca292f32307c4e9f4194a5ff
Joseph Werle <joseph.werle@gmail.com>
2017-11-09 15:25:58 -0500

    set swarm defaults

--- swarm.js
+++ swarm.js
@@ -9,12 +9,18 @@ const debug = require('debug')('littlstar:cfs:swarm')
 /**
  * Creates a CFS discovery network swarm
  */
-async function createCFSDiscoverySwarm({cfs, id, key}) {
+async function createCFSDiscoverySwarm({cfs, id, key, dns = {}, dht = {}}) {
   key = normalizeCFSKey(key)
   cfs = cfs || await createCFS({id, key})
   id = id || cfs.id
   const swarm = discovery(cfs, {
-    dns: {domain: 'cfs.local'}
+    dns: {
+      domain: dns.domain || 'cfs.local',
+      server: dns.server || 'dns.littlstar.com',
+    },
+    dht: {
+      bootstrap: dht.bootstrap || 'dht.littlstar.com:6881',
+    }
   })
   return swarm
 }
