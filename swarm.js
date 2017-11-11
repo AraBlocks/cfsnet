5de7205698ede9c87c0ab22f5cd2312af7a42f8e
Joseph Werle <joseph.werle@gmail.com>
2017-11-11 12:40:30 -0500

    fix dht hostname

--- swarm.js
+++ swarm.js
@@ -8,12 +8,31 @@ const debug = require('debug')('littlstar:cfs:swarm')
 
 /**
  * Creates a CFS discovery network swarm
+ *
+ * @public
+ * @param {?(Object)} opts
+ * @param {?(Object)} opts.cfs
+ * @param {?(Object)} opts.key
+ * @param {?(Object)} opts.id
+ * @param {?(Object)} opts.download
+ * @param {?(Object)} opts.upload
+ * @param {?(Object)} opts.dht
+ * @param {?(Object)} opts.dns
+ * @return {HyperDiscovery}
  */
-async function createCFSDiscoverySwarm({cfs, id, key, dns = {}, dht = {}}) {
+async function createCFSDiscoverySwarm({
+  cfs = null,
+  key = null,
+  id = null,
+  download = true,
+  upload = true,
+  dns = {},
+  dht = {}
+} = {}) {
   key = normalizeCFSKey(key)
   cfs = cfs || await createCFS({id, key})
-  id = id || cfs.id
   const swarm = discovery(cfs, {
+    upload, download,
     dns: {
       domain: dns.domain || 'Littlstar.local',
       server: dns.server || 'dns-service.us-east-1.littlstar.com',
