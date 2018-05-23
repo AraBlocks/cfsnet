48e8151a67ca6d8cad1fe7297c7e47cf4a88688b
Madeline Cameron <madeline.a.cameron@gmail.com>
2018-05-23 15:36:52 -0400

    fix(drive): Fix issue with rmdir import

--- drive.js
+++ drive.js
@@ -4,6 +4,7 @@ const { normalizeCFSKey } = require('./key')
 const hyperdrive = require('hyperdrive')
 const mkdirp = require('mkdirp')
 const rimraf = require('rimraf')
+const rmdir = require('rmdir')
 const debug = require('debug')('cfsnet:create:drive')
 
 async function createCFSDrive({
