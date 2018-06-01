d842559857c772377a5cda4e7f25bfaec487a69c
Joseph Werle <joseph.werle@gmail.com>
2018-06-01 16:49:04 -0400

    refactor(index.js): Remove requires from index.js

--- index.js
+++ index.js
@@ -2,19 +2,3 @@ console.warn(`
   Requiring the "index" (index.js) of the 'cfsnet' module is now deprecated".
   Please require the 'cfsnet' module directly (require('cfsnet/create')) instead.
 `)
-
-Object.assign(exports,
-  require('./agent'),
-  require('./key-path'),
-  require('./log'),
-  require('./create'),
-  require('./destroy'),
-  require('./drive'),
-  require('./drives'),
-  require('./env'),
-  require('./key'),
-  require('./remote'),
-  require('./sha256'),
-  require('./swarm'),
-  require('./tree')
-)
