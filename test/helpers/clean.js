c5e1618b24abf93be059ce1d9644a6b072d8b079
alex.lau <alex.lau@littlstar.com>
2018-06-20 12:24:13 -0400

    test(package.json) reset ava to run all tests style(clean.js) applied linter styles and debug instead of console.log

--- test/helpers/clean.js
+++ test/helpers/clean.js
@@ -1,17 +1,22 @@
 const fs = require('fs')
 const rimraf = require('rimraf')
+const debug = require('debug')('cfsnet:test')
 
 const txt = fs.readFileSync('test/helpers/cleanup.txt', 'utf8')
 const lines = txt.split(/\r?\n/)
 for (let i = 0; i < lines.length; i++) {
   const line = lines[i]
-  if (line)
+  if (line) {
     rimraf(line, (err) => {
-      if (err)
-        console.log(err)
+      if (err) {
+        debug(err)
+      }
     })
+  }
 }
-rimraf("cleanup.txt", (err) => {
-  if (err)
-    console.log(err)
+
+rimraf('cleanup.txt', (err) => {
+  if (err) {
+    debug(err)
+  }
 })
