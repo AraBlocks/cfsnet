172cfd2ae0ee6699fdf8f4368b8e2eb266fbf09c
Joseph Werle <joseph.werle@gmail.com>
2018-09-12 12:11:04 -0400

    refactor(drives.js): Clean up dead code

--- drives.js
+++ drives.js
@@ -1,17 +1,1 @@
-const { createCFSKeyPath } = require('./key-path')
-const debug = require('debug')('cfsnet:drive:map')
-
-Object.assign(exports, new class {
-  has(value) {
-    value = value || {}
-    const { id, key, cfs } = value
-    if (cfs && cfs.identifier && cfs.key) {
-      const path = createCFSKeyPath({ id: cfs.identifier, key: cfs.key })
-      return path in this
-    } else if (id) {
-      const path = createCFSKeyPath({ id, key })
-      return path in this
-    }
-    return false
-  }
-}())
+const debug = require('debug')('cfsnet:drives')
